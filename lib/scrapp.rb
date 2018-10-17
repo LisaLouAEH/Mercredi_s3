1b1f61067e6cb35746c198b239006a0605a18a4d
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-10-17 21:36:53 +0200

    config du programme pour scrapper d'autre departement que le val d'oise

--- lib/scrapp.rb
+++ lib/scrapp.rb
@@ -9,9 +9,10 @@ class Scrapper
     def initialize(url)
         @box = travesti(Nokogiri::HTML(open(url)))
     end
+
     def get_email(page)
         @email = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-        return @email
+        #return @email
     end
 
     def travesti(pages)
@@ -25,11 +26,13 @@ class Scrapper
         pages.css('a.lientxt').each do |node|
             tab_name << node.text
         end
+        #puts tab_name
         #------------------------------------------------------------------------------------------------
         #tab_url recupere tout les urls des pages qui contiennents les adresses emails
-        pages.css('//@href').grep(/95/).each do |node|
+        pages.css('//@href').grep(/34/).each do |node|
             tab_url << node.to_s.gsub("./", "http://annuaire-des-mairies.com/")
         end
+        
         #------------------------------------------------------------------------------------------------
         #tab-email recupere toute les adresses emails depuis le tableau d'url
         tab_url.each do |url|
@@ -52,8 +55,9 @@ class Scrapper
         #----------------------------------------------------------------------------------------------
         # fusionne les hash_emails et hash_city
         hash_scrap = Hash[hash_city.zip(hash_email)]
+        
         return hash_scrap
     end
     
 end
-    
+
